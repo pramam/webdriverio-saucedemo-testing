b549038abb00d77209a8c19f28b0c501e2f4174b
pramam <57686495+pramam@users.noreply.github.com>
2022-02-01 16:43:06 -0800

    example.e2e: Testing Logout functionality

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -8,6 +8,18 @@ describe('Sauce Demo', () => {
         await LoginPage.login('standard_user', 'secret_sauce');
         await expect(InventoryPage.secondaryTitle).toHaveTextContaining('PRODUCTS');
     });
+    it('should be able to logout after login', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('standard_user', 'secret_sauce');
+        await expect(InventoryPage.secondaryTitle).toHaveTextContaining('PRODUCTS');
+
+        const elMenuButton = await $('#react-burger-menu-btn')
+        await elMenuButton.click();
+        await browser.pause(5000);
+        const elLogoutLink = await $('#logout_sidebar_link')
+        await elLogoutLink.click();
+    });
 });
 
 
