864b38b08f944c0c0910b9083eb01749fcfa6537
pramam <57686495+pramam@users.noreply.github.com>
2022-02-04 12:00:58 -0800

    Reset_2: Add items to cart, Reset App State from Cart Page

--- test/specs/reset1.e2e.js
+++ test/specs/reset1.e2e.js
@@ -7,7 +7,7 @@ describe('UserStory: Reset Cart State', () => {
     //TODO: Investigate: Running all the tests together is causing second test to fail
     //      Do I need to logout after one test?
     // State machine info:
-    // X 1 A 2 2 15
+    // X 1 A 2 2 15 A
     it('Reset_1: should be able to add two items to cart and zero it out by Reset App State from burger menu on the Inventory Page', async()=> {
         await LoginPage.open();
         await LoginPage.login('standard_user', 'secret_sauce');
@@ -37,13 +37,44 @@ describe('UserStory: Reset Cart State', () => {
         console.log(`Reset_1: Confirmed number of items in cart is 0`);
         // Ensure on same page
         await InventoryPage.ensureOnPage();
-
     })
 
-    // Other Tests to write:
-    // Not specifying FirstName/Last Name/ZipCode on checkout should fail the checkout
-    // should be able to add more than one item to cart from inventory page
-    // should not be able to checkout with problem_user
+        // State machine info:
+    // X 1 A 2 2 4 B 15 B
+    it.only('Reset_2: should be able to add two items to cart and zero it out by Reset App State from burger menu on the Cart Page', async()=> {
+        await LoginPage.open();
+        await LoginPage.login('standard_user', 'secret_sauce');
+
+        await InventoryPage.ensureOnPage();
+
+        console.log(`Reset_2: Logged in`)
+ 
+        await InventoryPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
+        await InventoryPage.addItemToCart('#add-to-cart-sauce-labs-bike-light');
+        console.log("Reset_2: Added 2 items to cart");
+ 
+        // This is done on Inventory page
+        await CartPage.checkNumCartItems(2);
+
+        await CartPage.clickOnCartIcon();
+
+        await CartPage.ensureOnPage();
+
+        // const elMenuButton = await $('#react-burger-menu-btn')
+        // await elMenuButton.click();
+        // await browser.pause(5000);
+        // const elLogoutLink = await $('#logout_sidebar_link')
+        // TODO: Getting: Request failed with status 400 element not interactable
+        // await elLogoutLink.click();
+        // await LoginPage.logout();
+        await LoginPage.resetAppState();
+        console.log(`Reset_2: resetAppState from Cart page`);
+
+        await CartPage.checkNumCartItems(0);
+        console.log(`Reset_2: Confirmed number of items in cart is 0`);
+        // Ensure on same page
+        await CartPage.ensureOnPage();
+    })
 });
 
 
