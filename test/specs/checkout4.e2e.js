79efb75eeba7e5193f77833070d955f5b6389c77
pramam <57686495+pramam@users.noreply.github.com>
2022-02-10 13:14:00 -0800

    Removed console.log and moved test steps into multiple describe blocks

--- test/specs/checkout4.e2e.js
+++ test/specs/checkout4.e2e.js
@@ -5,52 +5,68 @@ const CheckoutStep1Page = require ('../pageobjects/checkoutstep1.page');
 const CheckoutStep2Page = require('../pageobjects/checkoutstep2.page');
 const CheckoutCompletePage = require ('../pageobjects/checkoutcomplete.page');
 
-describe('UserStory: Checkout', () => {
-
-    //TODO: Investigate: Running all the tests together is causing second test to fail
-    //      Do I need to logout after one test?
-
+describe('UserStory: Checkout: Should error out when customer information is missing', () => {
     // State Machine:
     // X 1 A 2 4 B 6 C <incomplete cust info> <should see error>
-    it("TestCase_4: should error out when customer information is missing", async ()=>{ 
-        await LoginPage.open();
-        await LoginPage.login('standard_user', 'secret_sauce');
- 
-        await InventoryPage.ensureOnPage();
-
-        console.log(`TestCase_4: Logged in`)
- 
-        await InventoryPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
-
-        console.log("TestCase_4: Added item to cart");
-
-        // This check  is done on inventory page
-        await CartPage.checkNumCartItems(1);
-        await CartPage.clickOnCartIcon();
-
-        await CartPage.ensureOnPage();
-        // Checking count again on next page.
-        await CartPage.checkNumCartItems(1);
-        console.log("TestCase_4: Checked 1 item in cart, clicked on Cart Icon,ensured On CartPage");
-      
-        await CartPage.clickOnCheckout();
-        console.log("TestCase_4: Clicked on Checkout from CartPage");
-        
-        await CheckoutStep1Page.ensureOnPage();
-        console.log("TestCase_4: Ensured on CheckoutStep1Page");
- 
-        await CartPage.checkNumCartItems(1);
- 
-        
-        await CheckoutStep1Page.fillInCustomerInfo("", "Doe", "90210");
-        console.log("TestCase_4: CheckoutStep1Page: Filled in customer info");
-        
-        // This should throw an error as customer info is missing
-        await CheckoutStep1Page.clickOnContinue();
-        await CheckoutStep1Page.validateCustomerSeesError();
-
-        
-        // TODO: Reset App State for next test
+    describe(`Login`, ()=>{
+        it(`should login user`, async ()=>{
+            await LoginPage.open();
+            await LoginPage.login('standard_user', 'secret_sauce');
+        })
+    })
+    describe(`Inventory Page`, ()=>{
+        it(`should be on Inventory Page`, async ()=>{
+            await InventoryPage.ensureOnPage();
+        })
+    })
+    describe(`Add 1 item to cart`, ()=>{
+        it(`should add 1 item to cart`, async ()=>{
+            await InventoryPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
+        })
+        it(`cart should have 1 item`, async ()=>{
+            await CartPage.checkNumCartItems(1);
+        })
+    })
+    describe(`Click on Cart Icon`, ()=>{
+        it(`click on Cart Icon`, async ()=>{
+            await CartPage.clickOnCartIcon();
+        })
+    })
+    describe(`Cart Page`, ()=>{
+        it(`should be on Cart Page`, async ()=>{
+            await CartPage.ensureOnPage();
+        })
+        it(`should have 1 item in cart`, async ()=>{
+            await CartPage.checkNumCartItems(1);
+        })
+    })
+    describe(`Click on Checkout`, ()=>{
+        it(`click on Checkout`, async ()=>{
+            await CartPage.clickOnCheckout();
+        })
+    })
+    describe(`Checkout Step1 Page`, ()=>{
+        it(`should be on Checkout Step1 Page`, async ()=>{
+            await CheckoutStep1Page.ensureOnPage();
+        })
+        it(`should have 1 item in cart`, async ()=>{
+            await CartPage.checkNumCartItems(1);
+        })
+    })
+    describe(`Don't fill all customer info`, ()=>{
+        it(`fill in customer info with missing first name`, async ()=>{
+            await CheckoutStep1Page.fillInCustomerInfo("", "Doe", "90210");
+        })
+    })
+    describe(`Click on Continue`, () => {
+        it(`click on Continue`, async ()=> {
+            await CheckoutStep1Page.clickOnContinue();
+        })
+    })
+    describe(`Validate customer sees error`, ()=>{
+        it(`should see error`, async ()=>{
+            await CheckoutStep1Page.validateCustomerSeesError();
+        })
     })
 });
 
