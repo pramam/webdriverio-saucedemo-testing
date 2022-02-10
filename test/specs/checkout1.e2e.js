43a5b325f25da4476ff6b388f61f95e3dffcb974
pramam <57686495+pramam@users.noreply.github.com>
2022-02-10 10:44:18 -0800

    Added multiple describe blocks, removed console.log

--- test/specs/checkout1.e2e.js
+++ test/specs/checkout1.e2e.js
@@ -5,38 +5,41 @@ const LoginData = require('../../data/logindata');
 const allureReporter = require('@wdio/allure-reporter').default;
 
 
-describe('UserStory: Checkout', () => {
+describe('UserStory: Checkout: Add one item to cart and checkout successfully', () => {
+    allureReporter.addFeature('Checkout');
 
     //TODO: Investigate: Running all the tests together is causing second test to fail
     //      Do I need to logout after one test?
     // State machine info:
     // X 1 A 2 3
-    it('TestCase_1: should be able to add one item to cart and remove it, add it again from inventory page', async()=> {
-        allureReporter.addFeature('Checkout');
-        let stepnum = 0;
-        await LoginPage.open();
-        await LoginPage.login(LoginData.userName, LoginData.password);
+    describe('Login', ()=>{
+        it(`should login user ${LoginData.userName}`, async ()=> {
+            await LoginPage.open();
+            await LoginPage.login(LoginData.userName, LoginData.password);
+        })
+    })
+    describe('Inventory Page', ()=> {
+        it(`should ensure user is on Inventory Page`, async ()=> {
+            await InventoryPage.ensureOnPage();
+        })
+        it(`should ensure the Inventory page has the right content`, async ()=>{
 
-        await InventoryPage.ensureOnPage();
+        })
+    })
 
-        stepnum += 3;
-        console.log(`TestCase_1: S${stepnum}: Logged in`)
- 
-        await CartPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
-        await CartPage.checkNumCartItems(1);
-        stepnum += 2;
-        console.log(`TestCase_1: S${stepnum}: Added item to cart`);
-
-        await CartPage.removeItemFromCart("#remove-sauce-labs-backpack");
-        await CartPage.checkNumCartItems(0);
-        stepnum += 2;
-        console.log(`TestCase_1: S${stepnum}: Removed item successfully`);
-
-        await CartPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
-        await CartPage.checkNumCartItems(1);
-        stepnum += 2;
-        console.log(`TestCase_1: S${stepnum}: Added removed item to cart again`);
-        console.log(`TestCase_1: S${stepnum} END`);
+    describe(`Add, Remove, Add item to cart`, ()=>{
+        it(`should add item to cart`, async ()=>{
+            await CartPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
+            await CartPage.checkNumCartItems(1);
+        })
+        it(`should remove item from cart`, async ()=>{
+            await CartPage.removeItemFromCart("#remove-sauce-labs-backpack");
+            await CartPage.checkNumCartItems(0);
+        })
+        it(`should add item to cart again`, async ()=>{
+            await CartPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
+            await CartPage.checkNumCartItems(1);
+        })
     })
 });
 
