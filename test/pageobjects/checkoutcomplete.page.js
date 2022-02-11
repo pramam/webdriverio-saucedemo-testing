4e6891ebb929205a7fd7b7fcfd963ee61fcc63c2
pramam <57686495+pramam@users.noreply.github.com>
2022-02-10 17:45:48 -0800

    Check title on CheckoutComplete.ensureOnPage()

--- test/pageobjects/checkoutcomplete.page.js
+++ test/pageobjects/checkoutcomplete.page.js
@@ -4,15 +4,30 @@ const Page = require('./page');
  * sub page containing specific selectors and methods for a specific page
  */
 class CheckoutCompletePage extends Page {
-
+    /**
+     * define selectors using getter methods
+     */
+    get secondaryTitle() {
+        return $('#header_container > div.header_secondary_container > span')
+    }
+    get secondaryHeader(){
+        return $("h2")
+    }
     async ensureOnPage(){
         if (await browser.getUrl() !== "https://www.saucedemo.com/checkout-complete.html")
             throw Error("CheckoutCompletePage.ensureOnPage: Not on correct page")
-        
-        const elHeaderOnCheckout = await $("h2")
+     
+        let title = 'CHECKOUT: COMPLETE!';
+        let actualTitle = await this.secondaryTitle.getText();
+        if ( actualTitle !== title)
+            throw Error(`CheckoutCompletePage.ensureOnPage: title ${actualTitle} does not match expected title ${title}`);
 
-        if (await elHeaderOnCheckout.getText() !== "THANK YOU FOR YOUR ORDER")
-            throw Error ("CheckoutCompletePage.ensureOnPage: Did not find text THANK YOU FOR YOUR ORDER")
+        const elHeaderOnCheckout = await $("h2")
+        let secHeader = "THANK YOU FOR YOUR ORDER";
+        let actualSecHeader = await this.secondaryHeader.getText();
+
+        if (actualSecHeader !== secHeader)
+            throw Error (`CheckoutCompletePage.ensureOnPage: Header ${actualSecHeader} does not match expected header ${secHeader}`)
     }
     async clickOnBackHome(){
         console.log("CheckoutCompletePage: To implement clickOnBackHome");
