ea85a76ad53a68b3c6b327fbcfc97c7ccf5b01a3
pramam <57686495+pramam@users.noreply.github.com>
2022-02-10 17:35:42 -0800

    Check title on CheckoutStep2.ensureOnPage()

--- test/pageobjects/checkoutstep2.page.js
+++ test/pageobjects/checkoutstep2.page.js
@@ -4,12 +4,25 @@ const Page = require('./page');
  * sub page containing specific selectors and methods for a specific page
  */
 class CheckoutStep2Page extends Page {
+    /**
+     * define selectors using getter methods
+     */
+     get secondaryTitle() {
+        return $('#header_container > div.header_secondary_container > span')
+    }
+
+    
 
     async ensureOnPage(){
         let url = await browser.getUrl();
 
         if (url !== "https://www.saucedemo.com/checkout-step-two.html")
             throw Error(`CheckoutStep2Page.ensureOnPage: Not on correct page on page ${url}`)
+
+        let title = 'CHECKOUT: OVERVIEW';
+        let actualTitle = await this.secondaryTitle.getText();
+        if ( actualTitle !== title)
+            throw Error(`CheckoutStep2Page.ensureOnPage: title ${actualTitle} does not match expected title ${title}`);
     }
     async clickOnFinish(){
         // Click on FINISH button
