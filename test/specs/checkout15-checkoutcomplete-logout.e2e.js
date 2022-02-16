f73c981ddf87c9a4b3aef192654152688dc448e5
pramam <57686495+pramam@users.noreply.github.com>
2022-02-16 15:49:28 -0800

    TestCase_15: One describe and one it block with no console logs

--- test/specs/checkout15-checkoutcomplete-logout.e2e.js
+++ test/specs/checkout15-checkoutcomplete-logout.e2e.js
@@ -8,79 +8,40 @@ const LoginData = require('../../data/logindata');
 const allureReporter = require('@wdio/allure-reporter').default;
 
 describe('UserStory: Logout', () => {
-
-    //TODO: Investigate: Running all the tests together is causing second test to fail
-    //      Do I need to logout after one test?
-
+    allureReporter.addFeature('Logout');
 
     // This is TestCase_2 modified with logout-login from CheckoutComplete page
     // Log back in and make sure there are 0 items in cart
     // State machine info:
     // X 1 A 2 4 B 6 C 17 9 D 12 E 14(Logout) Y 1(Login) A
     it("TestCase_15: (TestCase_2 modified) should be able to add one item to cart, checkout, logout at checkoutComplete page, log back in and check 0 items in cart", async ()=>{ 
-        allureReporter.addFeature('Logout');
-        let stepnum = 0;
         await LoginPage.open();
         await LoginPage.login(LoginData.userName, LoginData.password);
  
-        await InventoryPage.ensureOnPage();
-        stepnum += 3;
-        console.log(`TestCase_15: S${stepnum} Logged in`)
- 
+        await InventoryPage.ensureOnPage(); 
         await CartPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
-        stepnum += 1;
-        console.log(`TestCase_15: S${stepnum} Added item to cart`);
-
-        // This check  is done on inventory page
         await CartPage.checkNumCartItems(1);
 
         await CartPage.clickOnCartIcon();
         await CartPage.clickOnCheckout();
-        await CheckoutStep1Page.ensureOnPage();
-        stepnum += 4;
-        console.log(`TestCase_15: S${stepnum} On CheckoutStep1Page`);
 
+        await CheckoutStep1Page.ensureOnPage();
         await CartPage.checkNumCartItems(1);
- 
         await CheckoutStep1Page.fillInCustomerInfo("Jane", "Doe", "90210");
-        stepnum += 2;
-        console.log(`TestCase_15: S${stepnum} CheckoutStep1Page: Filled in customer info`);
-        
         await CheckoutStep1Page.clickOnContinue();
-        stepnum += 1;
-        console.log(`TestCase_15: S${stepnum} CheckoutStep1Page: Clicked on Continue`);    
         
         await CheckoutStep2Page.ensureOnPage();
-        stepnum += 1;
-        console.log(`TestCase_15: S${stepnum} CheckoutStep2Page: ensure on page`);    
-
         await CheckoutStep2Page.clickOnFinish();
 
         await CheckoutCompletePage.ensureOnPage();
-        stepnum += 2;
-        console.log(`TestCase_15: S${stepnum} CheckoutCompletePage: ensure on page`);    
-
         await CartPage.checkNumCartItems(0);
         await LoginPage.logout();
-        stepnum += 2;
-        console.log(`TestCase_15: S${stepnum} Logged out`);
-
         await LoginPage.ensureOnLogoutPage();
-
-        // I should be on a login page
-        // await LoginPage.open();
+        // I should be on a login page again
         await LoginPage.login(LoginData.userName, LoginData.password);
-        stepnum += 2;
-        console.log(`TestCase_15: S${stepnum} Logged in again`);
 
         await InventoryPage.ensureOnPage();
-        stepnum += 1;
-        console.log(`TestCase_15: S${stepnum} InventoryPage.ensure`);
         await CartPage.checkNumCartItems(0);
-        stepnum += 1;
-        console.log(`TestCase_15: S${stepnum} Check 0 items in cart`);
-        
-        console.log(`TestCase_15: S${stepnum} END`);
     })
 
 });
