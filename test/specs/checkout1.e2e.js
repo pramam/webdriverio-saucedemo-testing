fd1beff3376ba6d6339086b7c7b7a14690a2c3da
pramam <57686495+pramam@users.noreply.github.com>
2022-02-16 14:50:41 -0800

    Redoing TestCase 1 & 2: One describe, one it block

--- test/specs/checkout1.e2e.js
+++ test/specs/checkout1.e2e.js
@@ -3,34 +3,24 @@ const InventoryPage = require('../pageobjects/inventory.page');
 const CartPage = require('../pageobjects/cart.page');
 const LoginData = require('../../data/logindata');
 const allureReporter = require('@wdio/allure-reporter').default;
-const {padDigits} = require('../../utils/myUtils');
 
-describe('UserStory: Checkout: Add one item to cart, remove it, add it again', () => {
+describe('UserStory: Checkout', () => {
     allureReporter.addFeature('Checkout');
     // State machine info:
     // X 1 A 2 3
-    let step = 0;
-    step++;
-    it(`${padDigits(step,3)} Login: should login user ${LoginData.userName}`, async ()=> {
+
+    it(`TestCase_1: Add one item to cart, remove it, add it again`, async ()=>{
         await LoginPage.open();
         await LoginPage.login(LoginData.userName, LoginData.password);
-    })
-    step++;
-    it(`${padDigits(step,3)} Inventory Page: should be on Inventory Page`, async ()=>{
+
         await InventoryPage.ensureOnPage();
-    })
-    step++;
-    it(`${padDigits(step,3)} Add,Remove,Add: should add item to cart`, async ()=>{
+
         await CartPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
         await CartPage.checkNumCartItems(1);
-    })
-    step++;
-    it(`${padDigits(step,3)} Add,Remove,Add: should remove item from cart`, async ()=>{
+
         await CartPage.removeItemFromCart("#remove-sauce-labs-backpack");
         await CartPage.checkNumCartItems(0);
-    })
-    step++;
-    it(`${padDigits(step,3)} Add,Remove,Add: should add item to cart again`, async ()=>{
+
         await CartPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
         await CartPage.checkNumCartItems(1);
     })
--- test/specs/checkout2.e2e.js
+++ test/specs/checkout2.e2e.js
@@ -6,88 +6,38 @@ const CheckoutStep2Page = require('../pageobjects/checkoutstep2.page');
 const CheckoutCompletePage = require ('../pageobjects/checkoutcomplete.page');
 const LoginData = require('../../data/logindata');
 const allureReporter = require('@wdio/allure-reporter').default;
-const {padDigits} = require('../../utils/myUtils');
 
-describe('UserStory: Checkout : Add one item to cart and check it out successfully', () => {
+describe('UserStory: Checkout', () => {
     allureReporter.addFeature('Checkout');
-    let step = 0;
 
     // State machine info:
     // X 1 A 2 4 B 6 C 9 D 12 E
-    step++;
-    it(`${padDigits(step,3)} Login: should login user`, async ()=>{
+    it(`TestCase_2: Add one item to cart and check it out successfully`, async ()=>{
         await LoginPage.open();
         await LoginPage.login(LoginData.userName, LoginData.password);
-    })
-    step++;
-    it(`${padDigits(step,3)} Inventory Page: should be on Inventory Page`, async ()=> {
+    
         await InventoryPage.ensureOnPage();
-    })
-    step++;
-    it(`${padDigits(step,3)} Add 1 item to cart: should add 1 item to cart`, async ()=> {
+    
         await CartPage.addItemToCart('#add-to-cart-sauce-labs-backpack');
-    })
-    step++;
-    it(`${padDigits(step,3)} Add 1 item to cart: cart should have 1 item`, async ()=>{
         await CartPage.checkNumCartItems(1);
-    })
-    step++;
-    it(`${padDigits(step,3)} Click on Cart Icon: click on Cart Icon`, async ()=>{
         await CartPage.clickOnCartIcon();
-    })
-    step++;
-    it(`${padDigits(step,3)} Cart Page: should be on Cart Page`, async ()=>{
         await CartPage.ensureOnPage();
-    })
-    step++;
-    it(`${padDigits(step,3)} Cart Page: should have 1 item in Cart`, async ()=>{
         await CartPage.checkNumCartItems(1);
-    })
-    step++;
-    it(`${padDigits(step,3)} Click on Checkout: click on Checkout`, async ()=>{
         await CartPage.clickOnCheckout();
-    })
-    step++;
-
-    it(`${padDigits(step,3)} Checkout Step1 Page: should be on Checkout Step1 Page`, async()=>{
+    
         await CheckoutStep1Page.ensureOnPage();
-    })
-    step++;
-    it(`${padDigits(step,3)} Checkout Step1 Page: should have 1 item in cart`, async ()=>{
         await CartPage.checkNumCartItems(1);
-    })
-    step++;
-    it(`${padDigits(step,3)} Checkout Step1 Page: fill in customer info`, async ()=>{
         await CheckoutStep1Page.fillInCustomerInfo("Jane", "Doe", "90210");
-    })
-    step++;
-    it(`${padDigits(step,3)} Checkout Step1 Page: validate customer info`, async ()=>{
         await expect(CheckoutStep1Page.firstName).toHaveValue('Jane');
         await expect(CheckoutStep1Page.lastName).toHaveValue('Doe');
         await expect(CheckoutStep1Page.postalCode).toHaveValue('90210');
-    })
-    step++;
-    it(`${padDigits(step,3)} Click on Continue: click on Continue`, async ()=>{
         await CheckoutStep1Page.clickOnContinue();
-    })
-    step++;
-    it(`${padDigits(step,3)} Checkout Step2 Page: should be on Checkout Step2 Page`, async ()=>{
+        
         await CheckoutStep2Page.ensureOnPage();
-    })
-    step++;
-    it(`${padDigits(step,3)} Checkout Step2 Page: should have 1 item in cart`, async ()=>{
         await CartPage.checkNumCartItems(1);
-    })
-    step++;
-    it(`${padDigits(step,3)} Click on Finish: click on Finish`, async ()=>{
         await CheckoutStep2Page.clickOnFinish();
-    })
-    step++;
-    it(`${padDigits(step,3)} Checkout Complete Page: should be on Checkout Complete page`, async ()=>{
+        
         await CheckoutCompletePage.ensureOnPage();
-    })
-    step++;
-    it(`${padDigits(step,3)} Checkout Complete Page: cart should be empty`, async ()=>{
         await CartPage.checkNumCartItems(0);
     })
 });
