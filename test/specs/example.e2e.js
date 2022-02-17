d4cfeda05ed26c900e974c191b9da3ade213e77f
pramam <57686495+pramam@users.noreply.github.com>
2022-02-16 17:06:37 -0800

    One describe block with one it block, renamed test case to match state diagram

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -3,37 +3,24 @@ const InventoryPage = require('../pageobjects/inventory.page');
 const LoginData = require('../../data/logindata');
 const allureReporter = require('@wdio/allure-reporter').default;
 
-describe('Login: Should be able to login with valid credentials', () => {
+describe('UserStory: Login', () => {
     allureReporter.addFeature('Login');
-    describe(`Login`, ()=> {
-        it(`should login user`, async ()=>{
-            await LoginPage.open();
-            await LoginPage.login(LoginData.userName, LoginData.password);
-        })
-    })
-    describe(`Inventory Page`, ()=>{
-        it(`should be on Inventory Page`, async ()=>{
-            await expect(InventoryPage.secondaryTitle).toHaveTextContaining('PRODUCTS');
-        })
+    it(`Login_1: Should be able to login with valid credentials`, async ()=> {
+        await LoginPage.open();
+        await LoginPage.login(LoginData.userName, LoginData.password);
+        await expect(InventoryPage.secondaryTitle).toHaveTextContaining('PRODUCTS');
     })    
 });
 
-describe(`Login Logout: Should be able to login, then logout`, ()=>{
-    describe(`Login`, ()=>{
-        it(`should login user`, async ()=>{
-            await LoginPage.open();
-            await LoginPage.login(LoginData.userName, LoginData.password);
-        })
-    })
-    describe(`Inventory Page`, ()=>{
-        it(`should be on Inventory Page`, async ()=>{
-            await expect(InventoryPage.secondaryTitle).toHaveTextContaining('PRODUCTS');
-        })
-    })
-    describe(`Logout`, ()=>{
-        it(`should logout user`, async ()=>{
-            await LoginPage.logout();
-        })
+describe(`UserStory: Login`, ()=>{
+    allureReporter.addFeature('Login');
+    it(`Login_2: Should be able to login, then logout`, async ()=>{
+        await LoginPage.open();
+        await LoginPage.login(LoginData.userName, LoginData.password);
+
+        await expect(InventoryPage.secondaryTitle).toHaveTextContaining('PRODUCTS');
+
+        await LoginPage.logout();
     })
 })
 
