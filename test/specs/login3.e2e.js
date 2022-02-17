f7b82be8e47718677a4f8e06f78c3f7909cd0dbc
pramam <57686495+pramam@users.noreply.github.com>
2022-02-16 16:32:43 -0800

    Login_3: One describe and one it block with no console.logs

--- test/specs/login3.e2e.js
+++ test/specs/login3.e2e.js
@@ -4,29 +4,17 @@ const LoginData = require('../../data/logindata');
 const allureReporter = require('@wdio/allure-reporter').default;
 
 describe("UserStory: Login", ()=>{
+    allureReporter.addFeature('Login');
     // State machine:
     // X 1 A 14 1 A
-    it('Login_3:should be able to login, logout, login', async () => {
-        allureReporter.addFeature('Login');
-        let stepnum = 0;
-
+    it('Login_3:should be able to login, logout, login', async () => {    
         await LoginPage.open();
-
         await LoginPage.login(LoginData.userName, LoginData.password);
         await InventoryPage.ensureOnPage();
-        stepnum += 3;
-        console.log(`Login_3: S${stepnum} Logged in successfully`);
-
-        await LoginPage.logout();
-        stepnum += 1;
-        console.log(`Login_3: S${stepnum} Logged out successfully`);
         
-        await LoginPage.open();
-
+        await LoginPage.logout();
+        // I should be on login page
         await LoginPage.login(LoginData.userName, LoginData.password);
         await InventoryPage.ensureOnPage();
-        stepnum += 3;
-        console.log(`Login_3: S${stepnum} Logged in again successfully`);
-        console.log(`Login_3: S${stepnum} END`);
     });
 })
